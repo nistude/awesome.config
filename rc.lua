eb6932ada9e719c50f784d23bdcd2ffb552c4aaa
Nikolay Sturm <sturm@erisiandiscord.de>
2011-07-27 07:46:29 +0200

    remove pidgin stuff

--- rc.lua
+++ rc.lua
@@ -384,8 +384,6 @@ awful.rules.rules = {
     { rule = { class = "Mysql-workbench-bin" }, properties = { floating = true } },
     -- Set Opera to always map on tags number 2 of screen 1.
     { rule = { class = "Opera" }, properties = { tag = tags[1][2] } },
-    { rule = { class = "Pidgin" }, properties = { floating = true } },
-    { rule = { class = "Pidgin", name = "Buddy List" }, properties = { floating = false } },
     { rule = { class = "Unison" }, properties = { floating = true } }
 }
 -- }}}
