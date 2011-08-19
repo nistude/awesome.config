1c9fd29a43277eecfc43ba52a80d73beb405311c
Nikolay Sturm <sturm@erisiandiscord.de>
2011-08-19 15:49:20 +0200

    add a pomodoro timer widget
    based on a tea timer from Joerg Thalheim
    http://comments.gmane.org/gmane.comp.window-managers.awesome/7182

--- rc.lua
+++ rc.lua
@@ -137,6 +137,81 @@ vicious.register(netwidget_up, vicious.widgets.net, '${eth0 up_kb}/${wlan0 up_kb
 upicon = widget({ type = "imagebox" })
 upicon.image = image(beautiful.widget_net_up)
 
+-- pomodoro timer widget
+pomodoro = {}
+-- tweak these values in seconds to your liking
+pomodoro.pause_duration = 300
+pomodoro.work_duration = 1200
+
+pomodoro.pause_title = "Pause finished."
+pomodoro.pause_text = "Get back to work!"
+pomodoro.work_title = "Pomodoro finished."
+pomodoro.work_text = "Time for a pause!"
+pomodoro.working = true
+pomodoro.left = pomodoro.work_duration
+pomodoro.widget = widget({ type = "textbox" })
+pomodoro.timer = timer { timeout = 1 }
+
+function pomodoro:settime(t)
+  if t >= 3600 then -- more than one hour!
+    t = os.date("%X", t-3600)
+  else
+    t = os.date("%M:%S", t)
+  end
+  self.widget.text = string.format("Pomodoro: <b>%s</b>", t)
+end
+
+function pomodoro:notify(title, text, duration, working)
+  naughty.notify {
+    bg = "#ff0000",
+    fg = "#aaaaaa",
+    title = title,
+    text  = text,
+    timeout = 10
+  }
+
+  pomodoro.left = duration
+  pomodoro:settime(duration)
+  pomodoro.working = working
+end
+
+pomodoro:settime(pomodoro.work_duration)
+
+pomodoro.widget:buttons(
+  awful.util.table.join(
+    awful.button({ }, 1, function()
+      pomodoro.last_time = os.time()
+      pomodoro.timer:start()
+    end),
+    awful.button({ }, 2, function()
+      pomodoro.timer:stop()
+    end),
+    awful.button({ }, 3, function()
+      pomodoro.timer:stop()
+      pomodoro.left = pomodoro.work_duration
+      pomodoro:settime(pomodoro.work_duration)
+    end)
+))
+
+pomodoro.timer:add_signal("timeout", function()
+  local now = os.time()
+  pomodoro.left = pomodoro.left - (now - pomodoro.last_time)
+  pomodoro.last_time = now
+
+  if pomodoro.left > 0 then
+    pomodoro:settime(pomodoro.left)
+  else
+    if pomodoro.working then
+      pomodoro:notify(pomodoro.work_title, pomodoro.work_text,
+	pomodoro.pause_duration, false)
+    else
+      pomodoro:notify(pomodoro.pause_title, pomodoro.pause_text,
+        pomodoro.work_duration, true)
+    end
+    pomodoro.timer:stop()
+  end
+end)
+
 -- weather widget
 weatherwidget = widget({ type = "textbox" })
 vicious.register(weatherwidget, vicious.widgets.weather, "${sky} ${tempc}°C", 1800, "EDDM")
@@ -222,6 +297,7 @@ for s = 1, screen.count() do
         mylayoutbox[s],
         mytextclock,
  	separator, weatherwidget,
+	separator, pomodoro.widget,
 	has_battery and separator,
 	has_battery and batbar.widget,
 	has_battery and batwidget,
