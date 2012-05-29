e3daa4b31c39ad20c5c56e0e4f6c434e248411a3
Nikolay Sturm <sturm@erisiandiscord.de>
2012-05-29 09:54:59 +0200

    reorg work setup

--- rc.lua
+++ rc.lua
@@ -440,12 +440,10 @@ globalkeys = awful.util.table.join(
             -- Setup terminal on current tag
 	    awful.util.spawn("roxterm -T mutt@work -e mutt")
 	    awful.util.spawn("roxterm -T mutt@home -e ssh -t nst.homeunix.net mutt")
-            awful.util.spawn("roxterm -T campfire_notify -e /home/sturm/bin/campfire-libnotify.py")
 	    awful.util.spawn("pidgin")
 
 	    -- Rule sets tag
-	    awful.util.spawn("opera")
-	    awful.util.spawn("firefox")
+	    awful.util.spawn("google-chrome")
 	end),
     awful.key({ modkey }, "b", function () scratch.drop(terminal .. " -e vim /home/sturm/braindump", nil, nil, 0.5) end),
     -- awful.key({ modkey }, "l", function () scratch.drop(terminal .. " -e vim /home/sturm/logbuch", nil, nil, 0.5) end),
